6a3bfd3f70e2aee3ab4258aba1712de3b3060544
DaniaDI <107719471+DaniaDI@users.noreply.github.com>
2023-04-25 22:57:25 +0300

    Update script.js

--- project_software1/project_software1/script.js
+++ project_software1/project_software1/script.js
@@ -1,19 +1,40 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
- const apiKey="716220c4a0c656fa0a1987c670e6dac0";
- const apiUrl="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//       .then(response => response.json())
+//       .then(json => console.log(json))
+//  const apiKey="716220c4a0c656fa0a1987c670e6dac0";
+//  const apiUrl="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
 
- const searchBox=document.querySelector(".content .search_bar input");
- const searchBtn=document.querySelector(".content .search_bar button");
- async function checkWeather(city){
-      const response= await fetch(apiUrl + city + '&appid=${apiKey}');
-      var data =await response.json();
-      
-      console.log(data);
+//  const searchBox=document.querySelector(".content .search_bar input");
+//  const searchBtn=document.querySelector(".content .search_bar button");
+//  async function checkWeather(city){
+//       const response= await fetch(apiUrl + city + '&appid=${apiKey}');
+//       var data =await response.json();
 
- }  
- searchBtn.addEventListener("click",()=>{
-      checkWeather(searchBox.value);
- })
- checkWeather();
+//       console.log(data);
+
+//  }  
+//  searchBtn.addEventListener("click",()=>{
+//       checkWeather(searchBox.value);
+//  })
+//  checkWeather();
+/*********************************************** */
+
+document.querySelector("main").innerHTML = mainHTML
+
+const searchBtn = document.getElementById("searchForm")
+const searchInput = document.getElementById("searchInput")
+
+searchBtn.addEventListener("submit", async (e) => {
+     e.preventDefault()
+     console.log(searchInput.value)
+
+     const data = await fetchData(searchInput.value)
+     renderMain(data.articles)
+
+})
+
+async function Search(query) {
+     const data = await fetchData(query)
+     renderMain(data.articles)
+}
+/***اعملوا function fetch data وحطوا شغلكو */
